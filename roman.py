80a61675c0a190785dcd40708a86c2914c642d54
ShilatoEmerson <46932293+ShilatoEmerson@users.noreply.github.com>
2019-06-06 11:25:41 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,7 @@
+def num(n):
+  a=["o","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+  x=n
+  if x in a:
+    print(a.index(x))
+n=str(input())
+num(n)
