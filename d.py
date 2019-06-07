dfff4bb87a527bcfe428f2232b975cd0f8db4424
ShilatoEmerson <46932293+ShilatoEmerson@users.noreply.github.com>
2019-06-07 12:39:43 +0530

    Update d.py

--- d.py
+++ d.py
@@ -6,5 +6,8 @@ m.sort()
 for i in range (len (m) -1):
   if m[i] == m[i+1] and m[i] not in s:
     s.append(m[i])
-s.sort()
-print (' '.join(s))
+if(len(s)==0):
+  print("unique")
+else:
+  s.sort()
+  print (' '.join(s))
